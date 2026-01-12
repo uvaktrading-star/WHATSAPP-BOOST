421798d9890207fe2ea900d04673cc1775463265
uvaktrading-star <uvaktrading@gmail.com>
2026-01-12 05:01:54 -0800

    Update register.js

--- api/register.js
+++ api/register.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
         const newUser = new User({
             phone,
             password,
-            coins: 1 
+            coins: 10
         });
 
         await newUser.save();
